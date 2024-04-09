75d493036826d43031993c0601e40863f7bceb0d
tanaya-k <tanayakaundal2021@gmail.com>
2024-04-09 00:02:43 -0700

    Created Home page

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,22 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
     <style>
-    @import url('https://fonts.cdnfonts.com/css/jsmath-cmr10');
+      @import url('https://fonts.cdnfonts.com/css/jsmath-cmmi10');
     </style>
+      <h1>hi, i'm</h1>
+      <h1 className='home intro'>
+        Tanaya
+      </h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <footer>
+          Scroll to learn more...
+        </footer>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Name
-      </p>
     </>
   )
 }
