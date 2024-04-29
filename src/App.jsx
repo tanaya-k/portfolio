25bbe42639f019e1356e5816f6b35cfdb932e42e
tanaya-k <tanayakaundal2021@gmail.com>
2024-04-28 21:02:03 -0700

    Refactored Home as a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import './App.css'
+import Home from './components/home/home';
 // import { Navbar } from './components/navbar/navbar'
 
 function App() {
-
-  return (
-    <>
-    <style>
-      @import url('https://fonts.cdnfonts.com/css/jsmath-cmmi10');
-    </style>
-    {/* <Navbar /> */}
-    <div>
-      <h1>hi, i'm</h1>
-      <h1 className='home intro'>
-        Tanaya
-      </h1>
-    </div>
-        <footer>
-          Swipe to learn more...
-        </footer>
-    </>
-  )
+  return <Home />;
 }
 
 export default App
---
+++ src/components/home/home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../../App.css";
+
+function Home() {
+    return (
+    <>
+    <style>
+      @import url('https://fonts.cdnfonts.com/css/jsmath-cmmi10');
+    </style>
+    {/* <Navbar /> */}
+    <div>
+      <h1>hi, i'm</h1>
+      <h1 className='home intro'>
+        Tanaya
+      </h1>
+    </div>
+    <footer>
+      Swipe to learn more...
+    </footer>
+    </>
+    )
+}
+
+export default Home
