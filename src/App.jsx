993f91fb77e0d660288a7e829fee37a5f7545bf5
tanaya-k <tanayakaundal2021@gmail.com>
2024-04-29 20:47:53 -0700

    Implemented React router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css'
 import Home from './components/home/home';
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom";
 // import { Navbar } from './components/navbar/navbar'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/ >
+  },
+]);
+
+
+
 function App() {
-  return <Home />;
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App
