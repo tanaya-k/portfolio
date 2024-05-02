f55fa83b711956f78be84d71dbee0fd3bf12d008
tanaya-k <tanayakaundal2021@gmail.com>
2024-05-01 23:52:14 -0700

    Refactored About Me router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import './App.css'
-import Home from './components/home/home';
 import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+import Home from './components/home/home';
+import ErrorPage from './components/error_page/error_page';
+import AboutMe from './components/about_me/about_me';
 // import { Navbar } from './components/navbar/navbar'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/ >
+    element: <Home/>
+  },
+  {
+    path: "/about-me",
+    element: <AboutMe/>,
+    errorElement: <ErrorPage/>
   },
 ]);
-
 
 
 function App() {
