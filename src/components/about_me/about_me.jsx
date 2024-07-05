41137bfab36fd8994b8b14add682fca8e295a4f1
tanaya-k <tanayakaundal2021@gmail.com>
2024-07-05 14:02:01 -0700

    Refactored component name for project button

--- src/components/about_me/about_me.jsx
+++ src/components/about_me/about_me.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./about_me.css";
-import Project from "../project/project";
+import Project_Button from "../project_button/project_button";
 import Navbar from "../navbar/navbar";
 import projects from "../../../data/projects.json";
 
@@ -14,12 +14,12 @@ export default function AboutMe() {
         <p>I am a 3rd year student at the University of British Columbia, 
             studying Mathematics and currently enrolled in the Computational Mathematics Science co-op. 
             My technical interests include <b>full stack web development</b>, <b>machine learning
-                 research</b> and <b>mathematics</b>.
+                research</b> and <b>mathematics</b>.
         </p>
         <div className="projects display">
             {projects.map((project, id) => {
-            return (<Link to={`/project/${project.title}`} state={{from: project}}>
-                        <Project title={project.title} description={project.description} skills={project.skills}/>
+            return (<Link key={id} to={`/project/${project.title}`} state={{from: project}}>
+                        <Project_Button title={project.title} description={project.description} skills={project.skills}/>
                     </Link>)})}
         </div>
     </div>
