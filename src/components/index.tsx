f8a94a769bcdaaab0cf788a62b9811f5372f3074
tanaya-k <tanayakaundal2021@gmail.com>
2024-12-17 12:06:00 -0800

    Streamlined Component imports with index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,7 @@
 import NavBar from "./NavBar/NavBar";
+import Carousel from "./Carousel/Carousel";
+import ProjectItem from "./ProjectItem/ProjectItem";
 
 export { NavBar };
+export { Carousel };
+export { ProjectItem };
