60e2b18b485cb163d5b31e8594a58a75114ded89
tanaya-k <tanayakaundal2021@gmail.com>
2024-12-17 11:48:29 -0800

    Edited Home view

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -6,12 +6,12 @@ function Home() {
     return (
         <>
         <div className='home title'>
-            <h1 className='home title greeting'>Hi, i'm</h1>
+            <h1 className='home title greeting'>hi, i'm</h1>
             <h2 className='home title name'>Tanaya</h2>
-            <footer>
-                <Link className='home link' to='/about-me'>Click to see more...</Link>
-            </footer>
         </div>
+        <footer>
+            <Link className='home link' to='/about-me'>Click to see more...</Link>
+        </footer>
         </>
     );
 }
